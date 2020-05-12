873ebd5b1c33ec8f7bf4aa96ffe48960f4e807dc
loliipopshock <22512825+lolipopshock@users.noreply.github.com>
2020-05-12 12:22:39 -0400

    Add join inference scripts

---
+++ scripts/test_joint_inference_stamp.sh
@@ -0,0 +1,4 @@
+python vis_joint_inference_stamp.py --gpu 0 \
+        --maskgen_script scripts/test_pretrained_box2mask_stamp.sh  \
+        --imggen_script scripts/test_pretrained_mask2image_stamp.sh \
+        --how_many 1000
