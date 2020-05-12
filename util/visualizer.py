ac8db0ba4faf75e03e3644edda382d4a0edcdce4
loliipopshock <22512825+lolipopshock@users.noreply.github.com>
2020-05-12 11:44:40 -0400

    Compatibility for PIllow in Python3

--- util/visualizer.py
+++ util/visualizer.py
@@ -7,6 +7,7 @@ import time
 from . import util
 from . import html
 import scipy.misc
+from PIL import Image
 try:
     from StringIO import StringIO  # Python 2.7
 except ImportError:
@@ -46,7 +47,7 @@ class Visualizer():
                     s = StringIO()
                 except:
                     s = BytesIO()
-                scipy.misc.toimage(image_numpy).save(s, format="jpeg")
+                Image.fromarray(image_numpy).save(s, format="jpeg")
                 # Create an Image object
                 img_sum = self.tf.Summary.Image(encoded_image_string=s.getvalue(), height=image_numpy.shape[0], width=image_numpy.shape[1])
                 # Create a Summary value
